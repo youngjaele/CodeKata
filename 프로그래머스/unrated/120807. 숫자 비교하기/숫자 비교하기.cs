dbfe9a84f2046abc2e7b6e83ee604c2a49461fe2
youngjaele <149346316+youngjaele@users.noreply.github.com>
2023-11-21 09:35:26 +0900

    [level unrated] Title: 숫자 비교하기, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120807. 숫자 비교하기/숫자 비교하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        if (num1 == num2)
+        {
+          return answer = 1;  
+        }
+        else 
+        {
+          return answer = -1;  
+        }
+    }
+}
