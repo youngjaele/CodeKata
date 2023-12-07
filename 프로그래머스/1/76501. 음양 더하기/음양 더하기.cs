1aeee9b471d54cc18540bbcdbed51715d0f3bd09
youngjaele <149346316+youngjaele@users.noreply.github.com>
2023-12-07 10:06:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cs
@@ -2,13 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int[] absolutes, bool[] signs) {
-        int answer = 0;
-        
-        for(int i = 0; i < absolutes.Length; i++)
-        {
-            answer += absolutes[i] = signs[i] ? absolutes[i] : absolutes[i]* -1;
-        }
-        
+        int answer = 123456789;
         return answer;
     }
 }
